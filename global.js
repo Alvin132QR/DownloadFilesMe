6b6613f9ba675a67dacc26c7b3cb531db9e58fbd
Alvin132QR <114194093+Alvin132QR@users.noreply.github.com>
2024-01-10 12:38:34 +0700

    Update global.js

--- global.js
+++ global.js
@@ -1,12 +1,12 @@
 
 
-var allowedDomains = ['https://doadfies.vercel.app/', 'direct-link.net/1062040/download-addon-minecraft', 'linkvertise.com', 'direct-link.net'];
+var allowedDomains = ['direct-link.net/1062040/download-addon-minecraft', 'linkvertise.com', 'direct-link.net'];
 var currentDomain = window.location.hostname;
 
 if (allowedDomains.includes(currentDomain)) {
   document.getElementById("downloadContent").style.display = "flex";
   document.getElementById("mainContent").style.display = "none";
-} else {
+} else if ('https://doadfies.vercel.app/'.includes(currentDomain)){
   document.getElementById("downloadContent").style.display = "none";
   document.getElementById("mainContent").style.display = "flex";
 }
