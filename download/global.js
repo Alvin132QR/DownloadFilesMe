8e6ac4523b57ca39f0879d38f4accec8d61015dc
Alvin132QR <114194093+Alvin132QR@users.noreply.github.com>
2024-01-10 19:41:43 +0700

    Update global.js

--- download/global.js
+++ download/global.js
@@ -1,12 +1,13 @@
 
 
 var allowedDomains = ['doadfies.vercel.app/download/main.html', 'linkgenie.me/s/KOryH', 'sub2unlock.com', 'linkgenie.me', 'direct-link.net/1062040/download-addon-minecraft', 'linkvertise.com', 'direct-link.net'];
-var notallowedDomains = ['doadfies.vercel.app'];
+var notallowedDomains = ['alvin132qr.github.io'];
 var currentDomain = window.location.hostname;
 
 if (allowedDomains.includes(currentDomain)) {
   document.getElementById("downloadContent").style.display = "flex";
-} else if (notallowedDomains.includes(currentDomain)){
+} 
+if (notallowedDomains.includes(currentDomain)) {
   document.getElementById("downloadContent").style.display = "none";
   var pr = document.createElement("pr");
   pr.textContent ="Use Linkvertise Or Directlink For Download";
