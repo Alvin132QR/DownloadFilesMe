ab13de3d31534cd7042882fc8d0dac0e5e903799
Alvin132QR <114194093+Alvin132QR@users.noreply.github.com>
2024-02-02 14:17:36 +0700

    Update global.js

--- download/global.js
+++ download/global.js
@@ -1,6 +1,6 @@
 
 
-var allowedDomains = ['doadfies.vercel.app/download/main.html', 'linkgenie.me/s/KOryH', 'sub2unlock.com', 'linkgenie.me', 'direct-link.net/1062040/download-addon-minecraft', 'linkvertise.com', 'direct-link.net'];
+var allowedDomains = ['doadfies.vercel.app/download/main.html', 'linkgenie.me/s/KOryH', 'sub2unlock.com', 'linkgenie.me', 'direct-link.net/1062040/download-addon-minecraft', 'linkvertise.com', 'direct-link.net', 'https://linkvertise.com/'];
 var notallowedDomains = ['alvin132qr.github.io', 'github.com'];
 var currentDomain = window.location.hostname;
 
